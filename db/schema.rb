0eac38fc4423bc0f45dbb7a3c0deeaacde79878f
lokideos <archmage09@gmail.com>
2021-08-27 21:40:17 +0300

    Add link-user relationship; assign a user to link during creating link via GQL

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_180158) do
+ActiveRecord::Schema.define(version: 2021_08_27_183312) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_08_27_180158) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "links", "users"
 end
