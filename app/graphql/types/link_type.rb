69ff386f26dfce4db62e36cc080de4e37ae51ddd
lokideos <archmage09@gmail.com>
2021-08-27 21:48:36 +0300

    Show votes for links

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -4,5 +4,6 @@ module Types
     field :url, String, null: false
     field :description, String, null: false
     field :posted_by, Types::UserType, null: true, method: :user
+    field :votes, [Types::VoteType], null: false
   end
 end
